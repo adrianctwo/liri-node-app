f866ab590221ba98cf30480d6f13ce671f3b445a
adrianctwo <adrian.e.situ@gmail.com>
2019-08-02 16:27:35 -0700

    Added NPM package and setting up user input into array

--- liri.js
+++ liri.js
@@ -1,4 +1,17 @@
 require("dotenv").config();
 
+// API Keys
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+// NPM package
+var axios = require("axios");
+var moment = require("moment");
+
+// Grabbing user input in array[3]
+var fs = require("fs");
+var query =process.argv[3];
+
+// Checking for keys
+var option = process.argv[2];
+console.log(option);
+
